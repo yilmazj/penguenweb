a4cf4260e7d6b876cc9067fd86562cd237e34625
yilmazj <yilmaztivi@gmail.com>
2026-02-01 13:37:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.open("penguin").then(cache =>
-      cache.match(e.request).then(res => res || fetch(e.request))
-    )
-  );
+const CACHE_NAME = ‘nihilist-penguin-v1’;
+const urlsToCache = [
+‘/nihilist-penguin.html’,
+‘/manifest.json’
+];
+
+self.addEventListener(‘install’, event => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
+});
+
+self.addEventListener(‘fetch’, event => {
+event.respondWith(
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+);
 });
